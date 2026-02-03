f8ad0c728b02f5072c703675519a3a54678b55a4
lifanwar <mualifulanwar1@gmail.com>
2026-02-03 17:37:09 +0200

    Deleteemail that i dont need it

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -34,11 +34,3 @@ CACHES = {
         'LOCATION': os.environ.get('REDIS_URL'),
     }
 }
-
-# Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
